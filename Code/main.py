7d38b458b7842a13b00af31e1c3d72d86e777c44
Irsa Ashraf <irsaashraf@irsas-mbp.mynetworksettings.com>
2025-07-20 16:14:00 -0400

    Raeding locations dynamically in main flask app

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,8 @@ Application to start the server
 '''
 
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 import os
 import utils 
 
@@ -16,25 +17,35 @@ def root():
 
 
 # endpoint that reads handball court data and renders a map 
-@app.route('/FindCourts', methods = ["GET"])
+@app.route('/FindCourts', methods = ["GET", "POST"])
 def find_courts():
     '''
     '''
 
-    nyc_lat = 40.7128
-    nyc_lon = -74.0060
+    default_lat = 40.7128
+    default_lon = -74.0060
+
+    if request.method == "POST":
+        try:
+            user_location = request.get_json()
+            user_lat = float(user_location.get('lat', default_lat))
+            user_lon = float(user_location.get('lon', default_lon))
+        except Exception as e:
+            user_lat, user_lon = default_lat, default_lon
+    else:
+        user_lat, user_lon = default_lat, default_lon
 
     current_directory = os.getcwd()
     data_dir = os.path.join(os.path.dirname(current_directory), 'Data')
     
     hb_data = pd.read_csv(os.path.join(data_dir, "handball_courts_clean.csv"))
 
-    nyc_map = utils.create_map(hb_data)
+    nyc_map = utils.create_map(hb_data, user_lat, user_lon)
 
     # Save the map as an HTML file
     nyc_map.save('templates/map.html')
 
-    return render_template('index.html')
+    return render_template('map.html')
 
 
 
